561baa767576800fa884a2566f87fe1d64bcc975
wissam alkja <26311459+wissamalkja@users.noreply.github.com>
2020-07-05 17:15:32 +0000

    Add files via upload

---
+++ serv-test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+        
+)
+
+func main() {
+
+
+
+    ////server
+    
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, r.URL.Path[1:])
+        
+    })
+
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hi")
+        
+    })
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+    
+
+
+
+}
